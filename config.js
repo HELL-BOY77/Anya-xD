de3f1a0dcfc3637c079795d08f68698eec591732
HELL-BOY <107465084+HELL-BOY77@users.noreply.github.com>
2022-09-17 15:14:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,15 @@ global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'ᴅᴏɴᴇ',
+    success: 'ᴅᴏɴᴇ^_^',
     admin: 'ɢᴏʀᴜᴩ ᴀᴅᴍɪɴ ᴏɴʟʏ ᴜꜱᴇ!',
     botAdmin: 'ʙᴏᴛ ɪꜱ ɴᴏᴛ ᴀᴅᴍɪɴ!',
     owner: 'ᴏᴡɴᴇʀ ᴏɴʟʏ',
     group: 'ᴏɴʟʏ ꜰᴏʀ ɢʀᴏᴜᴩ',
     private: 'ᴏɴʟʏ ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ',
     bot: 'ʙᴏᴛ ɴᴜᴍʙᴇʀ ᴏɴʟʏ',
-    wait: '「▰▰▰▱▱▱」ʟᴏᴀᴅɪɴɢ...',
+    sticker: '*_ᴄʀᴇᴀᴛɪɴɢ ꜱᴛɪᴄᴋᴇʀ ᴩʟᴢᴢ ᴡᴀɪᴛ :)_*',
+    wait: '*「▰▰▰▱▱▱」ʟᴏᴀᴅɪɴɢ...*',
     endLimit: 'ᴛʜᴇ ʟɪᴍɪᴛ ɪꜱ ᴏᴠᴇʀ ᴩʟᴇᴀꜱᴇ ᴡᴀɪᴛ 24 ʜᴏᴜʀ ᴛᴏ ʀᴇꜱᴇᴛ ʟɪᴍɪᴛ',
 }
 //If you give a limit, don't give it a little bit 
